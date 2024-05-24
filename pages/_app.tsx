3e2973e9c53ff57c88169de6b13fd0762d62b348
Rick <rickvdkommer@gmail.com>
2024-05-24 23:24:55 +0200

    fix tab name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,42 +1,38 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
 import DesignV from "./design-v";
 import "./global.css";
-
+/*
+test
+*/
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    // Google Tag Manager script
-    const script = document.createElement('script');
-    script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-T8WWJBTD');`;
-    document.head.appendChild(script);
-  }, []);
-
   return (
     <Fragment>
       <Head>
-        <title>CollabMaps</title>
-        <link rel="icon" href="/faviconsquare.png" type="image/png" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
         
+
         {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FSE2SL24SV"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-B7P2MLQHH8"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-FSE2SL24SV');
+
+              gtag('config', 'G-B7P2MLQHH8');
             `,
           }}
+        />
+        
+        
+        <title>CollabMaps</title>
+        <link rel="icon" href="/faviconsquare.png" type="image/png" />
+
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
       <noscript>
@@ -44,7 +40,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
       </noscript>
       <DesignV />
-      <Component {...pageProps} />
     </Fragment>
   );
 }
