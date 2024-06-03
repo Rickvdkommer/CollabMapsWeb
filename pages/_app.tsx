5538e8e9499f21f60a52874fc868526be9fe9a22
Rick <rickvdkommer@gmail.com>
2024-06-03 14:46:42 +0200

    test2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,32 +3,41 @@ import Head from "next/head";
 import { Fragment } from "react";
 import DesignV from "./design-v";
 import "./global.css";
-import TagManager from 'react-gtm-module';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Head>
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FSE2SL24SV"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-B7P2MLQHH8');
-            `,
-          }}
-        />
-        
         <title>CollabMaps</title>
         <link rel="icon" href="/faviconsquare.png" type="image/png" />
 
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
 
-
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="CollabMaps" />
+        <meta property="og:description" content="The platform that bridges Content Creators with Businesses" />
+        <meta property="og:image" content="/opengraphimage.png" />
+        <meta property="og:url" content="https://www.yourleads.com" />
+        <meta property="og:type" content="website" />
       </Head>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-FSE2SL24SV"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-B7P2MLQHH8');
+          `,
+        }}
+      />
       <DesignV />
       <Component {...pageProps} />
     </Fragment>
