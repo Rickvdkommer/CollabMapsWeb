6552114b60be9ab6148ba049c46a8aa28e104546
Rick <rickvdkommer@gmail.com>
2024-06-03 17:09:35 +0200

    ADDED INDEX

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+// pages/index.tsx
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      <p>This is the homepage.</p>
+    </div>
+  );
+};
+
+export default HomePage;
