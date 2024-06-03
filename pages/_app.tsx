2687431d043e0f413eec1ea7c56f05ff18f43b9c
Rick <rickvdkommer@gmail.com>
2024-06-03 13:17:27 +0200

    update opengraphimage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,23 +17,31 @@ function MyApp({ Component, pageProps }: AppProps) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-
               gtag('config', 'G-B7P2MLQHH8');
             `,
           }}
         />
-          
-        
         
         <title>CollabMaps</title>
         <link rel="icon" href="/faviconsquare.png" type="image/png" />
 
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="CollabMaps" />
+        <meta property="og:description" content="Collaborative Mapping Tool" />
+        <meta property="og:image" content="/opengraphimage.png" />
+        <meta property="og:url" content="https://www.yourleads.com" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card Meta Tags (optional) */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="CollabMaps" />
+        <meta name="twitter:description" content="Collaborative Mapping Tool" />
+        <meta name="twitter:image" content="/twitterimage.png" />
       </Head>
       <DesignV />
+      <Component {...pageProps} />
     </Fragment>
   );
 }
