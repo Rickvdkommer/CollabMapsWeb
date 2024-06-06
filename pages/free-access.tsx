7dc4fcfd74181d77e99bba8cc2887225418d518e
Rick <rickvdkommer@gmail.com>
2024-06-06 22:40:11 +0200

    A/B test 2

--- pages/free-access.tsx
+++ pages/free-access.tsx
@@ -1,21 +1,12 @@
-// pages/free-access.tsx
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import { Fragment } from "react";
-import DesignV from "./design-v2";
-import "./global.css";
-import Script from 'next/script';
+// pages/index.tsx
+import React from 'react';
 
 const FreeAccessPage = () => {
   return (
-    <Fragment>
-    
-      <div>
-        <h1>Collabmaps bridges the gap between Businesses and Content Creators</h1>
-        <p>This is the free-access page.</p>
-      </div>
-    <DesignV />
-    </Fragment>
+    <div>
+      <h1>Collabmaps bridges the gap between Businesses and Content Creators</h1>
+      <p>This is the homepage.</p>
+    </div>
   );
 };
 
