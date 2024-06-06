8fab7f7f8d9991bab64975f2421fa20f84c5f928
Rick <rickvdkommer@gmail.com>
2024-06-06 19:07:12 +0200

    facebook pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import DesignV from "./design-v";
 import "./global.css";
 import Script from 'next/script';
 
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
@@ -19,7 +18,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta name="description" content="CollabMaps is the platform that allows Content Creators to find Businesses using our location-based find feature." />
         <link rel="canonical" href="https://www.collabmaps.com" />
-
 
         <meta property="og:title" content="CollabMaps" />
         <meta property="og:description" content="Connecting Content Creators with Businesses" />
@@ -43,6 +41,30 @@ function MyApp({ Component, pageProps }: AppProps) {
           `,
         }}
       />
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s) {
+              if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1448694002434561');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img height="1" width="1" style={{ display: 'none' }}
+             src="https://www.facebook.com/tr?id=1448694002434561&ev=PageView&noscript=1"
+        />
+      </noscript>
       <DesignV />
     </Fragment>
   );
