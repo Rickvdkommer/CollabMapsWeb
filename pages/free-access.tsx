bff862ac88d1b54ebde0a101d1e86c03677edab8
Rick <rickvdkommer@gmail.com>
2024-06-06 20:30:46 +0200

    a/b again

---
+++ pages/free-access.tsx
@@ -0,0 +1,13 @@
+// pages/index.tsx
+import React from 'react';
+
+const FreeAccessPage = () => {
+  return (
+    <div>
+      <h1>Collabmaps bridges the gap between Businesses and Content Creators</h1>
+      <p>This is the homepage.</p>
+    </div>
+  );
+};
+
+export default FreeAccessPage;
